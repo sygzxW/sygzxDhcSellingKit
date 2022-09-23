3e2433f7d2e94b4ee82694672d2d7f98e1d38170
sygzxW <73584462+sygzxW@users.noreply.github.com>
2022-09-23 23:30:24 +0200

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,46 +1,74 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "sygzx's dhc selling kit", HidePremium = true, IntroEnabled = false})
 
-local mainTab = Window:MakeTab({
-	Name = "scripts",
+local dhcTab = Window:MakeTab({
+	Name = "dhc selling",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
-mainTab:AddButton({
-	Name = "Crostide selling gui",
+dhcTab:AddButton({
+	Name = "Crostide Selling Gui",
 	Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Crostide/cdhc/main/gui"))()
   	end    
 })
-mainTab:AddButton({
-	Name = "Crostide cash counter",
+dhcTab:AddButton({
+	Name = "Crostide Cash Counter",
 	Callback = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Crostide/Counter/main/Cash"))()
   	end    
 })
-mainTab:AddButton({
-	Name = "halloween selling gui",
+dhcTab:AddButton({
+	Name = "Halloween Selling Gui",
 	Callback = function()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/halloweevn/SpookyControl/main/Source%20%3AD/GUI'))()
   	end    
 })
-mainTab:AddButton({
-	Name = "swagmode (v)",
-	Callback = function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/SwagModeV002'))() 
-  	end    
-})
-mainTab:AddButton({
-	Name = "BetterDaHood crasher",
+dhcTab:AddButton({
+	Name = "BetterDaHood Crasher",
 	Callback = function()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/BetterDaHood/BetterDaHoodCrasher/main/Crash'))()
   	end    
 })
+local mainTab = Window:MakeTab({
+	Name = "main scripts",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
 mainTab:AddButton({
-	Name = "infinite yield fe",
+	Name = "Swagmode (v)",
+	Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/SwagModeV002'))() 
+  	end    
+})
+mainTab:AddButtom({
+	Name = "Plasma (v)",
+	Callback = function()
+	loadstring(game:HttpGet("https://iexploit.xyz/scripts/plasma/loader"))()
+	end
+})
+mainTab:AddButtom({
+	Name = "Faded (v)",
+	Callback = function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/sygzxW/sygzxDhcSellingKit/main/customScripts/faded.lua"))()
+	end
+})
+local commandsTab = Window:MakeTab({
+	Name = "commands script",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+commandsTab:AddButton({
+	Name = "Infinite Yield FE",
 	Callback = function()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
   	end    
+})
+commandsTab:AddButtom({
+	Name = "CMD-X",
+	Callback = function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source",true))()
+	end
 })
 local creditsTab = Window:MakeTab({
 	Name = "credits",
